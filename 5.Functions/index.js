7db75f44c3af9c7e0eeed20bf16f534727642e49
venkat ravilla <venkatravilla333@gmail.com>
2025-08-25 09:48:59 +0530

    Scope

--- 5.Functions/index.js
+++ 5.Functions/index.js
@@ -73,26 +73,86 @@
 
 //ES-6 rest parameter
 
-var test = (...rest)=> {
+// var test = (...rest)=> {
  
-  console.log(rest, typeof rest)
-  console.log(Array.isArray(rest))
+//   console.log(rest, typeof rest)
+//   console.log(Array.isArray(rest))
   // console.log(rest[0])
   // console.log(rest[1])
   // console.log(rest[2])
   // console.log(rest[3])
   // console.log(rest[4])
-  var sum = 0
+//   var sum = 0
 
-  for (var value of rest) {
-    sum += value
-  }
-  console.log(sum)
-}
-test(1, 2, 3, 4, 5)
-
-
+//   for (var value of rest) {
+//     sum += value
+//   }
+//   console.log(sum)
+// }
+// test(1, 2, 3, 4, 5)
 
 
 
 
+
+
+//scope
+
+//global
+
+// var a = 1
+// let b = 2
+// const c = 3
+// console.log(a)
+// console.log(b)
+// console.log(c)
+
+// function test() {
+
+//   var d = 4
+//   let e = 5
+//   const f = 6
+  
+//   console.log(a)
+//   console.log(b)
+//   console.log(c)
+//   console.log(d)
+//   console.log(e)
+//   console.log(f)
+
+//   if (true) {
+//     var g = 7
+//     let h = 8
+//     const i = 9
+
+//   console.log(a)
+//   console.log(b)
+//   console.log(c)
+//   console.log(d)
+//   console.log(e)
+//   console.log(f)
+//   console.log(g)
+//   console.log(h)
+//   console.log(i)
+//   }
+//   console.log(g)
+//   // console.log(h)
+//   // console.log(i)
+// }
+
+// test()
+// console.log(d)
+// console.log(e)
+// console.log(f)
+// console.log(g)
+// console.log(h)
+// console.log(i)
+
+// var a = 10
+// console.log(a)
+// {
+// var b = 20
+// // console.log(a)
+// console.log(b)
+// }
+// console.log(b)
