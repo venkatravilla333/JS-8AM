e71a5853542c40e0f07c24a9b2538886cca0fa76
venkat ravilla <venkatravilla333@gmail.com>
2025-09-08 09:43:11 +0530

    arrays upto reduce

--- 7.Arrays/index.js
+++ 7.Arrays/index.js
@@ -101,6 +101,104 @@
 // console.log(combineArr)
 
 
+//How to loop array values
+
+// without using loop
+
+var arr = [1, 2, 3, 4, 5]
+
+// console.log(arr)
+// console.log(arr[0])
+// console.log(arr[1])
+// console.log(arr[2])
+// console.log(arr[4])
+
+// for (var value of arr) {
+//   console.log(value)
+// }
+
+//  arr.forEach((value, index) => {
+//   console.log(value*2)
+//   // return value
+// })
+
+//Copy
+
+// var a = 10
+
+// var b = a
+//  b = 20
+
+// console.log(a)
+// console.log(b)
+
+// import lodash from 'lodash'
+// var lodash = require('lodash')
+
+var originalArr = ['sachin', { road: 101 }, function () { }]
+
+
+// var copiedArr = originalArr //normal copy
+// var copiedArr = JSON.parse(JSON.stringify(originalArr)) //deep copy
+// var copiedArr = lodash.cloneDeep(originalArr) //deep copy
+// var copiedArr = Object.assign([], originalArr) //shallow copy(ES-5)
+// var copiedArr = [...originalArr] //shallow copy(ES-6)
+// var copiedArr = originalArr.slice() //shallow copy(ES-6)
+
+// copiedArr[0] = 'kohli'
+// copiedArr[1].road  = 201
+// console.log(originalArr)
+// console.log(copiedArr)
+
+// var arr = [1, 2, 3, 4, 5]
+
+// console.log(arr, typeof arr)
+
+// var str = arr.join('*')
+// console.log(str, typeof str)
+
+// var myarr = str.split()
+
+// console.log(myarr, typeof myarr)
+
+
+// var arr = [1, 2, 3, 4, 5]
+// var arr = [{name: 'sachin', age : 45}, {name: 'gill', age: 22}, {name: 'kohli', age: 35}]
+
+// var filteredRes = arr.filter((value) => {
+// return value > 3
+// })
+// var filteredRes = arr.filter((object) => {
+// return object.age > 30
+// })
+
+// console.log(arr)
+// console.log(filteredRes)
+
+
+// var arr = [1, 2, 3, 4, 5]
+
+
+// var mappedRes = arr.map((value) => {
+//  return  value * 100
+// })
+
+// console.log(arr)
+// console.log(mappedRes)
+
+
+// var arr = [1, 2, 3, 4, 5]
+
+// var reducedRes = arr.reduce((cum, cur) => {
+//    console.log(cum, cur)
+// }, 100)
+// // console.log(arr)
+// // console.log(reducedRes)
+
+
+
+
+
 
 
 
