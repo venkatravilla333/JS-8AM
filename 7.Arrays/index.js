16e2dbbd0753920938c961816bfb1843da70a54c
venkat ravilla <venkatravilla333@gmail.com>
2025-09-09 09:59:48 +0530

    arrays completed

--- 7.Arrays/index.js
+++ 7.Arrays/index.js
@@ -195,6 +195,121 @@ var originalArr = ['sachin', { road: 101 }, function () { }]
 // // console.log(arr)
 // // console.log(reducedRes)
 
+// How to remove duplicate values from array
+
+// var arr = [1, 2, 3, 4, 3, 5]
+// console.log(arr)
+
+// var uniqueArr = [...new Set(arr)]
+// console.log(uniqueArr)
+
+// var uniqueArr = []
+
+// function removeDuplicates(arr) {
+//   for (var value of arr) {
+//     console.log(value)
+//     if (uniqueArr.indexOf(value) === -1) {
+//      uniqueArr.push(value)
+//    }
+//   }
+// }
+
+// removeDuplicates(arr)
+
+//Flattening of array
+
+// var arr = [1, 2, 3, [10, 20], [100, 200, [1000, 2000]]]
+// console.log(arr)
+
+// var flattenArr = arr.flat(Infinity)
+// console.log(flattenArr)
+
+// var flattenArr = []
+
+// function flattenFun(arr) {
+//   for (var value of arr) {
+//     console.log(value)
+//     if (Array.isArray(value)) {
+//       flattenFun(value)
+//     } else {
+//       flattenArr.push(value)
+//     }
+//   }
+// }
+
+// flattenFun(arr)
+
+// console.log(flattenArr)
+
+// function test() {
+//   console.log(arguments)
+//   console.log(arguments, Array.isArray(arguments))
+
+//   var res = Array.from(arguments)
+//   console.log(res, Array.isArray(res))
+
+// }
+
+// test(1, 2, 3, 4, 5)
+
+
+// var arr = [2, 1, 5, 8, 4]
+// var arr = [2, 1, 15, 8, 4]
+var arr = ['sachin', 'dhoni', 'kohli']
+// var arr = [{course: 'node js'}, {course: 'react js'}, {course: 'javascipt'}]
+
+// console.log(arr)
+// console.log(arr.sort()) //ASO
+// console.log(arr.reverse())DSO
+
+// var DSO = arr.sort((a, b) => {
+//   if (a > b) {
+//     return -1
+//   } else {
+//     return 1
+//   }
+// })
+
+// console.log(DSO)
+
+// var ASO = arr.sort()
+// var DSO = arr.reverse()
+// console.log(DSO)
+
+// var ASO = arr.sort((obj1, obj2) => {
+//   if (obj1.course > obj2.course) {
+//     return 1
+//   } else {
+//     return -1
+//   }
+// })
+// console.log(ASO)
+
+// for (var i = 0; i < arr.length; i++){
+//   for (var j = i + 1; j < arr.length; j++){
+//     if (arr[i].course < arr[j].course) {
+//       var temp = arr[i]
+//       arr[i] = arr[j]
+//       arr[j] = temp
+//     }
+//   }
+// }
+
+// console.log(arr)
+
+// for (var i = 0; i < arr.length; i++){
+//   for (var j = i + 1; j < arr.length; j++){
+//     if (arr[i] < arr[j]) {
+//       var temp = arr[i]
+//       arr[i] = arr[j]
+//       arr[j] = temp
+//     }
+//   }
+// }
+
+// console.log(arr)
+
+
 
 
 
